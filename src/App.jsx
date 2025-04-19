e7569b247bf5d2ba1b0846216672861e1d909ae6
Alexandr Strelets <alexandrstriii@gmail.com>
2025-04-19 13:11:44 +0300

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import './App.scss';
 import { Sum } from './components/Sum/Sum';
+import { Person } from './components/Person/Person';
+
+const misha = {
+  name: 'Миша',
+  age: 30,
+  sex: 'm',
+  isMarried: true,
+  wife: 'Анна',
+};
+
+const anna = {
+  name: 'Анна',
+  age: 28,
+  sex: 'f',
+  isMarried: true,
+  husband: 'Миша',
+};
+
+const vasya = {
+  name: 'Вася',
+  sex: 'm',
+  isMarried: false,
+};
 
 export const App = () => (
   <>
@@ -9,5 +32,9 @@ export const App = () => (
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
+
+    <Person person={misha} />
+    <Person person={anna} />
+    <Person person={vasya} />
   </>
 );
